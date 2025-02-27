dede0a612c47149f92bafebd84867c4dce6e4888
legionJP <jjpsingh18@gmail.com>
2025-02-28 03:01:34 +0530

    Added the view , templates and url conf

--- OAuthWebSocket/OAuthWebSocketAPI/views.py
+++ OAuthWebSocket/OAuthWebSocketAPI/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+@api_view()
+def Oauth(request):
+    context = {
+    'data': 'data from google api'
+    }
+    return render(request, 'auth.html', context)
+    #return HttpResponse(template_name='home.html')            
